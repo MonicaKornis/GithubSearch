27830fdd35061f74ee39dc48163eb8ccd4f10fb5
monicakornis <2011mkornis@gmail.com>
2018-05-19 12:52:17 -0400

    add basic skeleton of app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import MainContainer from './Components/container';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/container.jsx
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class MainContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    console.log('hello');
+  }
+}
+
+export default MainContainer;
